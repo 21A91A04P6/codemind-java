e0c261fc307a423e054b107d3f138c54c1cc85d2
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-04-28 17:34:41 +0530

    import java.util.Scanner; class divison {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,add;         a=sc.nextInt();         b=sc.nextInt();         add=a+b;         System.out.println(+add);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class divison
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,add;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        add=a+b;
+        System.out.println(+add);
+    }
+}
