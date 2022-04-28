2d2d06e3e536c7ec5fe3d2cff4f375cb718b09d3
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-04-28 17:32:15 +0530

    import java.util.Scanner; class divison {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,d;         a=sc.nextInt();         b=sc.nextInt();         d=a%b;         System.out.println(+d);     } }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class divison
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,d;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        d=a%b;
+        System.out.println(+d);
+    }
+}
