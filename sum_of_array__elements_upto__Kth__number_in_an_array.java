82736c747419f45c2c044369a65aa3f8f4325a66
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-12-06 15:38:08 +0530

    import java.util.Scanner; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],i,s1=0,k;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]==k)             {             break;             }             else             {                 s1=s1+a[i];             }         }         System.out.printf("%d",s1+k);     } }

--- sum_of_array__elements_upto__Kth__number_in_an_array.java
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
-class up
+class sum
 {
     public static void main(String args[])
     {
-	Scanner sc=new Scanner(System.in);
-	int n,b,sum=0;
-	n=sc.nextInt();
-
-	int a[],i;
-	a=new int[n];
-	for(i=0;i<n;i++)
-	{
-	a[i]=sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int n,a[],i,s1=0,k;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==k)
+            {
+            break;
+            }
+            else
+            {
+                s1=s1+a[i];
+            }
+        }
+        System.out.printf("%d",s1+k);
     }
-    	b=sc.nextInt();
-    	for(i=0;i<b;i++)
-	{
-    	sum=sum+a[i];
-	} 
-    System.out.printf("%d",sum);
-}
 }
