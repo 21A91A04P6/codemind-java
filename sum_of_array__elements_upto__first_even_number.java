2524a72df991faf387fb850b573b20ccc17e3079
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-12-06 15:35:33 +0530

    import java.util.Scanner; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],i,s1=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(a[i]%2==0)             {             break;             }             else             {                 s1=s1+a[i];             }         }         System.out.printf("%d",s1);     } }

---
+++ sum_of_array__elements_upto__first_even_number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a[],i,s1=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+            break;
+            }
+            else
+            {
+                s1=s1+a[i];
+            }
+        }
+        System.out.printf("%d",s1);
+    }
+}
