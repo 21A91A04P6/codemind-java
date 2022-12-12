53cc3ad6566647a0246b61d4c4d86b05f5979df5
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-12-12 20:08:49 +0530

    import java.util.Scanner; class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],i,k=0,c,p=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             k=k+a[i];         }         c=k/n;         for(i=0;i<n;i++)         {             if(a[i]<=c)             {                 p++;             }         }         System.out.printf("%d",p);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a[],i,k=0,c,p=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            k=k+a[i];
+        }
+        c=k/n;
+        for(i=0;i<n;i++)
+        {
+            if(a[i]<=c)
+            {
+                p++;
+            }
+        }
+        System.out.printf("%d",p);
+    }
+}
