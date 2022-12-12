b015a0a4e365420073fb04dc98e49875e8eee22c
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-12-12 20:13:51 +0530

    import java.util.Scanner; class evod {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],i;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(a[i]%2==0)             {                 System.out.printf("%d ",a[i]);             }         }         for(i=0;i<n;i++)         {             if(a[i]%2!=0)             {                 System.out.printf("%d ",a[i]);             }         }     } }

---
+++ even_elements_followed_by_odd.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class evod
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a[],i;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                System.out.printf("%d ",a[i]);
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2!=0)
+            {
+                System.out.printf("%d ",a[i]);
+            }
+        }
+    }
+}
