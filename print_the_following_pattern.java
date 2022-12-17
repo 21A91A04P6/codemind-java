a11be99ddbc9cefd8f6717407cf5aa665a73b584
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-12-17 22:56:40 +0530

    import java.util.Scanner; class pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);          int i, j, n, ch = 'A';     n=sc.nextInt();     for (i =1; i<=n; i++)     {         for (j = 1; j<=n; j++)         {             System.out.printf ("%c ", ch);         }         ch++;         System.out.printf (" ");     }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
-class pate
+class pattern
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-    int i, j, n;  
+         int i, j, n, ch = 'A';  
     n=sc.nextInt();   
-    for (i=1; i<=n;i++)   
+    for (i =1; i<=n; i++)   
     {  
-        for (j=1;j<=n; j++)   
+        for (j = 1; j<=n; j++)   
         {  
-            if(i==j)
-            {
-                System.out.printf("0");
-            }
-            else
-            {
-            System.out.printf ("x");   
-        }  }
+            System.out.printf ("%c ", ch);   
+        }  
+        ch++;  
         System.out.printf ("
 ");  
     }  
-}  
+    }
 }
