43d11167f876f829eac04beed704b084db6f2a3d
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-12-17 22:44:46 +0530

    import java.util.Scanner; class pate {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);     int i, j, n;     n=sc.nextInt();     for (i=1; i<=n;i++)     {         for (j=1;j<=n; j++)         {             if(i==j)             {                 System.out.printf("0");             }             else             {             System.out.printf ("x");         }  }         System.out.printf (" ");     } } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
-class pat
+class pate
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-    int i, j,n;
-    n=sc.nextInt();
-    for(i=n;i>=1;i--)
-    {
-        for(j=1;j<=i;j++)
-        {
-            System.out.printf("%d",j);
-        }
-        System.out.printf("
-");
-    }
+    int i, j, n;  
+    n=sc.nextInt();   
+    for (i=1; i<=n;i++)   
+    {  
+        for (j=1;j<=n; j++)   
+        {  
+            if(i==j)
+            {
+                System.out.printf("0");
+            }
+            else
+            {
+            System.out.printf ("x");   
+        }  }
+        System.out.printf ("
+");  
+    }  
+}  
 }
-    }
